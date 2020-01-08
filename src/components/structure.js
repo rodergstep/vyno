b9e9386ff8108651877feef0d394992a65d70529
rodergstep <rodergstep@gmail.com>
2020-01-08 23:13:43 +0200

    bugfix2: case

--- src/components/structure.js
+++ src/components/structure.js
@@ -6,7 +6,7 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
-import SelectLanguage from "./electLanguage";
+import SelectLanguage from "./selectLanguage";
 import srcMainLogo from "../images/main-logo.png";
 
 const drawerWidth = 240;
