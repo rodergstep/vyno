94eec892314f3b8bd8fa3a8e97512a08cae863dd
rodergstep <rodergstep@gmail.com>
2020-04-06 22:10:04 +0300

    testing

--- src/utils/cart.service.js
+++ src/utils/cart.service.js
@@ -1,6 +1,8 @@
 export const getCart = () => {
   if (typeof window !== "undefined") {
     console.log("window ni underfined")
+    console.log(window)
+
     return JSON.parse(window.localStorage.getItem("cart"))
   } else {
     return []
@@ -8,6 +10,7 @@ export const getCart = () => {
 }
 export const updateCart = updatedCart => {
   if (typeof window !== "undefined") {
+    console.log(window)
     window.localStorage.setItem("cart", JSON.stringify(updatedCart))
   }
 }
